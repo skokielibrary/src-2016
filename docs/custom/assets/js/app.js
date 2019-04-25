255b21091162df22bec8f82cfcacbce1eed292e9
Stephen Delaney <sdelaney@skokielibrary.info>
2019-04-25 14:43:52 -0500

    create script for flashing lights

---
+++ docs/custom/assets/js/app.js
@@ -0,0 +1,9 @@
+window.onscroll = function(e) {
+  var imgReplace = document.getElementsByClassName("banner_image")[0];
+  vertical_position = this.scrollY;
+  if (170 < vertical_position && vertical_position < 425) {
+    imgReplace.src = "custom/assets/img/2019/header-lights-on.png";
+  } else {
+    imgReplace.src = "custom/assets/img/2019/header.png";
+  }
+}
