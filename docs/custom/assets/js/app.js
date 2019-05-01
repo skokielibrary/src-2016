306b9b774c31d1a0546ad8b3fda27e0068446fd8
Stephen Delaney <sdelaney@skokielibrary.info>
2019-05-01 16:11:23 -0500

    add log in button, adjust header css, change js vertical position detection

--- docs/custom/assets/js/app.js
+++ docs/custom/assets/js/app.js
@@ -1,7 +1,7 @@
 window.onscroll = function(e) {
   var imgReplace = document.getElementsByClassName("banner_image")[0];
   vertical_position = this.scrollY;
-  if (170 < vertical_position && vertical_position < 425) {
+  if (170 < vertical_position && vertical_position < 475) {
     imgReplace.src = "custom/assets/img/2019/header-lights-on.png";
   } else {
     imgReplace.src = "custom/assets/img/2019/header.png";
